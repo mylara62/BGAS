e7e0ecad6f60029928b1e58f1560611aef64f529
Shiv <shiv.yuvasoft@gmail.com>
2016-11-19 16:36:24 +0530

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,37 @@
+class ApplicationController < ActionController::Base
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+	#before_filter :authenticate_user!
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+		def is_systemmanager?  		
+	  	if current_user.role_id == Role.find_by_name('System Manager').id
+	  		return true
+	  	else
+	  		return false
+	  	end
+		end
+
+		def is_customer?  		
+	  	if current_user.role_id == Role.find_by_name('Customer').id
+	  		return true
+	  	else
+	  		return false
+	  	end
+		end
+
+		def is_manager?  		
+	  	if current_user.role_id == Role.find_by_name('Manager').id
+	  		return true
+	  	else
+	  		return false
+	  	end
+		end
+
+ protected
+
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :address, :phone_number, :globalaccessid,:role_id])
+	end
+end
